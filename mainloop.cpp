3d2084641d9b1e71385f1a4312d153a85f8e66cf
Brad Bishop <bradleyb@fuzziesquirrel.com>
2017-01-05 15:11:16 -0500

    Remove sensorcache.hpp
    No longer used.
    
    Change-Id: I4c97e0fcd8edf34b122876000802afe26876e569
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- mainloop.cpp
+++ mainloop.cpp
@@ -20,7 +20,6 @@
 #include <chrono>
 #include <algorithm>
 #include "sensorset.hpp"
-#include "sensorcache.hpp"
 #include "hwmon.hpp"
 #include "sysfs.hpp"
 #include "mainloop.hpp"
@@ -89,7 +88,6 @@ void MainLoop::run()
 {
     // Check sysfs for available sensors.
     auto sensors = std::make_unique<SensorSet>(_path);
-    auto sensor_cache = std::make_unique<SensorCache>();
 
     for (auto& i : *sensors)
     {
@@ -193,14 +191,6 @@ void MainLoop::run()
                     auto realIface = std::experimental::any_cast<std::shared_ptr<ValueObject>>
                                      (iface->second);
                     realIface->value(value);
-                }
-
-                // Update sensor cache.
-                if (sensor_cache->update(i.first, value))
-                {
-                    // TODO: Issue#4 - dbus event here.
-                    std::cout << i.first.first << i.first.second << " = "
-                              << value << std::endl;
                 }
             }
         }
