607f4dcf25bcce57affd6decbd583a74b1cec995
Brad Bishop <bradleyb@fuzziesquirrel.com>
2017-01-06 14:55:16 -0500

    Add threshold typename aliases
    Change-Id: I38b1891cd34249efef224264022f78f4159048d1
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- interface.hpp
+++ interface.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <xyz/openbmc_project/Sensor/Value/server.hpp>
+#include <xyz/openbmc_project/Sensor/Threshold/Warning/server.hpp>
+#include <xyz/openbmc_project/Sensor/Threshold/Critical/server.hpp>
 #include <sdbusplus/server.hpp>
 
 template <typename T>
@@ -8,10 +10,18 @@ using ServerObject = typename sdbusplus::server::object::object<T>;
 
 using ValueInterface = sdbusplus::xyz::openbmc_project::Sensor::server::Value;
 using ValueObject = ServerObject<ValueInterface>;
+using WarningInterface =
+    sdbusplus::xyz::openbmc_project::Sensor::Threshold::server::Warning;
+using WarningObject = ServerObject<WarningInterface>;
+using CriticalInterface =
+    sdbusplus::xyz::openbmc_project::Sensor::Threshold::server::Critical;
+using CriticalObject = ServerObject<CriticalInterface>;
 
 enum class InterfaceType
 {
     VALUE,
+    WARN,
+    CRIT,
 };
 
 // vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
