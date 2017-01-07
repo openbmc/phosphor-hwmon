258e2879daec291038ed3555429f096fbac33132
Brad Bishop <bradleyb@fuzziesquirrel.com>
2017-01-06 20:44:05 -0500

    Detect power current and energy sensors
    Change-Id: I058eb1f3093a583ef21dd4be01d3fa535131fbde
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- mainloop.cpp
+++ mainloop.cpp
@@ -46,6 +46,18 @@ static constexpr auto typeAttrMap =
         hwmon::type::cvolt,
         ValueInterface::Unit::Volts,
         -3),
+    std::make_tuple(
+        hwmon::type::ccurr,
+        ValueInterface::Unit::Amperes,
+        -3),
+    std::make_tuple(
+        hwmon::type::cenergy,
+        ValueInterface::Unit::Joules,
+        -6),
+    std::make_tuple(
+        hwmon::type::cpower,
+        ValueInterface::Unit::Watts,
+        -6),
 };
 
 using AttributeIterator = decltype(*typeAttrMap.begin());
