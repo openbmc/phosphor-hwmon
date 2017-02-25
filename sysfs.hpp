9ca659f96268c0db1a3a8fbaa85a5aa69e6fcae9
Brad Bishop <bradleyb@fuzziesquirrel.com>
2017-02-24 23:49:50 -0500

    Remove unused methods
    Change-Id: Ifcc8023f6e33c53e484497d6f90f4b117cf59072
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- sysfs.hpp
+++ sysfs.hpp
@@ -3,20 +3,6 @@
 #include <fstream>
 #include <string>
 
-template <typename T>
-void read_sysfs(const std::string& path, T& val)
-{
-    std::ifstream s(path);
-    s >> val;
-}
-
-template <typename T>
-void write_sysfs(const std::string& path, const T& val)
-{
-    std::ofstream s(path);
-    s << val;
-}
-
 inline std::string make_sysfs_path(const std::string& path,
                                    const std::string& type,
                                    const std::string& id,
