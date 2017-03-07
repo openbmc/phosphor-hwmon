14b22cb377d407550bbd32e7382e024469723cda
Matthew Barth <msbarth@us.ibm.com>
2017-03-07 15:46:59 -0600

    WIP: Create FanSpeed interface
    Create the FanSpeed interface when the fanx_target sysfs file is found,
    setting the initial target value to zero
    
    Change-Id: Ib4a099cff17cacea501b474969f292516db212b5
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- interface.hpp
+++ interface.hpp
@@ -3,6 +3,7 @@
 #include "xyz/openbmc_project/Sensor/Value/server.hpp"
 #include "xyz/openbmc_project/Sensor/Threshold/Warning/server.hpp"
 #include "xyz/openbmc_project/Sensor/Threshold/Critical/server.hpp"
+#include "xyz/openbmc_project/Control/FanSpeed/server.hpp"
 #include <sdbusplus/server.hpp>
 
 template <typename... T>
@@ -16,12 +17,16 @@ using WarningObject = ServerObject<WarningInterface>;
 using CriticalInterface =
     sdbusplus::xyz::openbmc_project::Sensor::Threshold::server::Critical;
 using CriticalObject = ServerObject<CriticalInterface>;
+using FanSpeedInterface =
+    sdbusplus::xyz::openbmc_project::Control::server::FanSpeed;
+using FanSpeedObject = ServerObject<FanSpeedInterface>;
 
 enum class InterfaceType
 {
     VALUE,
     WARN,
     CRIT,
+    FAN_SPEED,
 };
 
 // vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
