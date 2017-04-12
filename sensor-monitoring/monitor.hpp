f2debbc0869e252409bf315ed0d9dfb7d426f480
Matthew Barth <msbarth@us.ibm.com>
2017-04-12 13:48:29 -0500

    Add support for processing signal event triggers
    Process signal event triggers' list of conditions on a group and perform
    the defined actions.
    
    This re-uses the following struct directly from
    phosphor-inventory-manager:
    --struct PropertyChangedCondition
    
    Change-Id: Ic4a51fe2f50ab9087e1f2749f826fbe9dbfc8dd8
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- sensor-monitoring/monitor.hpp
+++ sensor-monitoring/monitor.hpp
@@ -30,12 +30,26 @@ class Monitor
                          const std::tuple<std::vector<std::shared_ptr<Event>>,
                                     std::vector<Action>>& eventDef);
 
+        using eventArg = std::tuple<Monitor*,
+                                    const SignalEvent*,
+                                    const std::tuple<
+                                        std::vector<std::shared_ptr<Event>>,
+                                        std::vector<Action>>*>;
+
     private:
         sdbusplus::bus::bus& bus;
 
         static const std::vector<
             std::tuple<std::vector<std::shared_ptr<Event>>,
                        std::vector<Action>>> events;
+
+        std::vector<std::unique_ptr<eventArg>> eventArgs;
+
+        std::vector<sdbusplus::server::match::match> matches;
+
+        static int handleSignal(sd_bus_message* msg,
+                                void* data,
+                                sd_bus_error* err);
 
 };
 
