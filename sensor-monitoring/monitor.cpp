5348eb0e0820d7585c354a39a92ca2dc77fef1d1
Matthew Barth <msbarth@us.ibm.com>
2017-04-12 11:33:39 -0500

    Create phosphor-sensor-monitor application
    Application is created allowing for a log_error action to be performed
    
    Change-Id: Iba22880b9e687167a9930e8540c61d4a4e6c18ff
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ sensor-monitoring/monitor.cpp
@@ -0,0 +1,26 @@
+#include <phosphor-logging/log.hpp>
+#include "monitor.hpp"
+
+namespace phosphor
+{
+namespace sensor
+{
+namespace monitoring
+{
+
+using namespace phosphor::logging;
+
+Monitor::Monitor(sdbusplus::bus::bus& bus) :
+    bus(bus)
+{
+
+}
+
+void Monitor::log_error(const char* msg)
+{
+    log<level::ERR>(msg);
+}
+
+} // namespace monitoring
+} // namespace sensor
+} // namespace phosphor
