59f49ed4329d3a16c52d35d9e1a19e59ca6bd241
Matthew Barth <msbarth@us.ibm.com>
2017-04-12 10:57:19 -0500

    Add sensor group monitoring data type framework
    Include supported data types for sensor groups:
    --Start event trigger
    --Signal event trigger
    --A countAtOrAbove condition
    --A log_error action
    
    Change-Id: I08afe0a80745341865e9adc20819bc55d5ff495b
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ sensor-monitoring/conditions.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <algorithm>
+#include "data_types.hpp"
+
+namespace phosphor
+{
+namespace sensor
+{
+namespace monitoring
+{
+namespace condition
+{
+
+template <typename T>
+auto countAtOrAbove(Group& sensors, const char* path, size_t count, T&& value)
+{
+    return [&sensors,
+            path,
+            count,
+            value = std::forward<T>(value)](T&& arg)
+    {
+        Group::iterator it =
+            std::find_if(sensors.begin(),
+                         sensors.end(),
+                         [&path](auto const& sensor)
+                         {
+                             return std::get<0>(sensor) == path;
+                         });
+        if (it != std::end(sensors))
+        {
+            std::get<1>(*it) = arg;
+        }
+        size_t condCount =
+            std::count_if(sensors.begin(),
+                          sensors.end(),
+                          [&value](auto const& sensor)
+                          {
+                              return std::get<1>(sensor) >= value;
+                          });
+        return condCount >= count;
+    };
+}
+
+} // namespace condition
+} // namespace monitoring
+} // namespace sensor
+} // namespace phosphor
