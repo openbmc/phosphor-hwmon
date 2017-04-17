2c632d2e46c3c688ba097e41fbf5647b3815633b
Matthew Barth <msbarth@us.ibm.com>
2017-04-17 15:49:37 -0500

    Base sensor monitoring generation script
    Sensor monitoring source generation script with mako template to
    generate an empty set of events.
    
    Change-Id: Ibbfd12cd691d46021c3f353b7a868b3d0f63693c
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ sensor-monitoring/gen-sensor-group-defs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import yaml
+from argparse import ArgumentParser
+from mako.template import Template
+
+
+def generate(yaml_file, output_file):
+    with open(yaml_file, 'r') as yaml_input:
+        yaml_data = yaml.safe_load(yaml_input) or {}
+
+    with open(output_file, 'w') as gen_out:
+        gen_out.write(Template(filename='generated.mako.cpp').render(
+            events=yaml_data))
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    # Groups of sensors and how they should be monitored yaml file
+    parser.add_argument("-y", "--yaml", dest="input_yaml",
+                        default=
+                        "example/sensor_monitoring_defs.yaml",
+                        help=
+                        "Input sensor monitoring definition yaml to parse")
+    parser.add_argument("-o", "--outdir", dest="output_dir",
+                        default=
+                        os.path.abspath('.'),
+                        help=
+                        "Output directory for source files generated")
+    args = parser.parse_args(sys.argv[1:])
+
+    # Verify given yaml file exists
+    yaml_file = os.path.abspath(args.input_yaml)
+    if not os.path.isfile(yaml_file):
+        print "Unable to find input yaml file " + yaml_file
+        exit(1)
+
+    generate(yaml_file, os.path.join(args.output_dir, "generated.cpp"))
