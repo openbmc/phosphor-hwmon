ec8edb178bd789a5ca61baeb8628e92640167e24
Matthew Barth <msbarth@us.ibm.com>
2017-04-17 12:22:50 -0500

    Documentation only, no functional change
    Added copyrights and comments within headers
    
    Change-Id: Iab5fa697444087c986b0a9aacca61f10c0dfa112
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- sensor-monitoring/actions.hpp
+++ sensor-monitoring/actions.hpp
@@ -13,6 +13,14 @@ namespace action
 
 using namespace phosphor::logging;
 
+/**
+ * @brief An action to log an error with the given message
+ *
+ * @param[in] msg - The message to log
+ *
+ * @return Lambda function
+ *     A lambda function to perform the log_error function
+ */
 inline auto log_error(const char* msg)
 {
     return [=](auto&, auto&)
