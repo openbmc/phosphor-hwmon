cbb4e1f2e5b0e654c36d0c7e227cb829fe7a4606
Matthew Barth <msbarth@us.ibm.com>
2017-04-17 12:22:50 -0500

    Documentation only, no functional change
    Added copyrights and comments within headers
    
    Resolves openbmc/openbmc#1343
    
    Change-Id: Iab5fa697444087c986b0a9aacca61f10c0dfa112
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- sensor-monitoring/actions.hpp
+++ sensor-monitoring/actions.hpp
@@ -9,6 +9,14 @@ namespace monitoring
 namespace action
 {
 
+/**
+ * @brief An action to log an error with the given message
+ *
+ * @param[in] msg - The message to log
+ *
+ * @return Lambda function
+ *     A lambda function to perform the log_error function
+ */
 inline auto log_error(const char* msg)
 {
     return [=](auto&, auto& mon)
--- sensor-monitoring/conditions.hpp
+++ sensor-monitoring/conditions.hpp
@@ -12,6 +12,22 @@ namespace monitoring
 namespace condition
 {
 
+/**
+ * @brief A condition used to trigger an action when a number of sensors are at
+ * or above a given value
+ * @details A given group of sensors is updated with their last known sensor
+ * value, which then the entire group is check if there are a given number of
+ * them at or above a value which would cause the condition to be true
+ *
+ * @param[in] sensors - Group of sensors
+ * @param[in] path - Path of a sensor within the group
+ * @param[in] count - Number of sensors needed at or above value
+ * @param[in] value - Value of sensors to be at or above
+ *
+ * @return Lambda function
+ *     A lambda function to determine if the number of sensors within the group
+ *     are at or above the given value
+ */
 template <typename T>
 auto countAtOrAbove(Group& sensors, const char* path, size_t count, T&& value)
 {
