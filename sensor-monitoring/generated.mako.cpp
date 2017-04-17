23508e9ac17121e8e26a7fb53c78e52dbf81c20a
Matthew Barth <msbarth@us.ibm.com>
2017-04-17 15:49:37 -0500

    Base sensor monitoring generation script
    Sensor monitoring source generation script with mako template
    
    Change-Id: Ibbfd12cd691d46021c3f353b7a868b3d0f63693c
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ sensor-monitoring/generated.mako.cpp
@@ -0,0 +1,25 @@
+## This file is a template, the comment below is emitted into the generated file
+/* This is an auto generated file. Do not edit. */
+#include "data_types.hpp"
+#include "monitor.hpp"
+#include "functor.hpp"
+#include "conditions.hpp"
+#include "actions.hpp"
+
+namespace phosphor
+{
+namespace sensor
+{
+namespace monitoring
+{
+
+const std::vector<std::tuple<std::vector<std::shared_ptr<Event>>,
+                             std::vector<Action>>>
+    Monitor::events
+{
+
+};
+
+} // namespace monitoring
+} // namespace sensor
+} // namespace phosphor
