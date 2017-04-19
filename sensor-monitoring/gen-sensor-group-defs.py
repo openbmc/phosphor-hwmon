136626f5ff0c6efe7d338eb4a7084333b2bbe1b1
Matthew Barth <msbarth@us.ibm.com>
2017-04-19 13:18:21 -0500

    WIP: Generate sensor group instances
    Create a static Group instance for each sensor group defined
    
    Change-Id: I3c2def49282ce00e6ed4dc49148384abd2ebee63
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- sensor-monitoring/gen-sensor-group-defs.py
+++ sensor-monitoring/gen-sensor-group-defs.py
@@ -11,9 +11,47 @@ def get_file(dir_path, filename):
     return os.path.join(dir_path, filename)
 
 
+def get_value_init(value_type):
+    return {
+        'int64_t': 0,
+        'bool': "false",
+        'string': ""
+    }[value_type]
+
+
+def get_condition_type(conditions, condition):
+    for c in conditions:
+        # Return the first instance of the condition found for its type
+        if c['name'] == condition:
+            return c['value']['type']
+
+
+def set_data_type(groups, egroup, value_type, value_init):
+    for g in groups:
+        if g['name'] in egroup:
+            g['value_type'] = value_type
+            g['value_init'] = value_init
+
+
+def set_group_data_type(yaml_data):
+    for e in yaml_data['events']:
+        s = set()
+        value_type = None
+        value_init = None
+        for t in e['triggers']:
+            for c in t['conditions']:
+                s.add(c)
+        for condition in s:
+            value_type = get_condition_type(yaml_data['conditions'], condition)
+            value_init = get_value_init(value_type)
+        set_data_type(yaml_data['groups'], e['groups'], value_type, value_init)
+
+
 def generate(yaml_file, output_file):
     with open(yaml_file, 'r') as yaml_input:
         yaml_data = yaml.safe_load(yaml_input) or {}
+
+    set_group_data_type(yaml_data)
 
     with open(output_file, 'w') as gen_out:
         gen_out.write(Template(filename='generated.mako.cpp').render(
--- sensor-monitoring/generated.mako.cpp
+++ sensor-monitoring/generated.mako.cpp
@@ -13,6 +13,17 @@ namespace sensor
 namespace monitoring
 {
 
+% for g in events["groups"]:
+static Group ${g['name']} = {
+    % for m in g['members']:
+    std::make_tuple(
+        "/xyz/openbmc_project/sensors/${str(g['type'])}/${str(m)}",
+        static_cast<${g['value_type']}>(${g['value_init']})
+    ),
+    % endfor
+};
+% endfor
+
 const std::vector<std::tuple<std::vector<std::shared_ptr<Event>>,
                              std::vector<Action>>>
     Monitor::events
