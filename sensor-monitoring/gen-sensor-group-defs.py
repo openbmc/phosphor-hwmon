a2dab79854105d718adf82aa9c9ddebdd3449e25
Matthew Barth <msbarth@us.ibm.com>
2017-04-19 13:18:21 -0500

    Generate sensor group instances
    Create a static Group instance for each sensor group defined
    
    Change-Id: I3c2def49282ce00e6ed4dc49148384abd2ebee63
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- sensor-monitoring/gen-sensor-group-defs.py
+++ sensor-monitoring/gen-sensor-group-defs.py
@@ -7,9 +7,49 @@ from argparse import ArgumentParser
 from mako.template import Template
 
 
+def get_value_type(value_type):
+    return {
+        'int64': "int64_t"
+    }.get(value_type)
+
+
+def get_value_init(value_type):
+    return {
+        'int64': 0,
+        'bool': "false",
+        'string': ""
+    }.get(value_type)
+
+
+def get_condition_type(conditions, condition):
+    for c in conditions:
+        if c['name'] == condition:
+            return c['parameters']['value']['type']
+    return None
+
+
+def set_group_value_data(groups, egroups, value_type):
+    for g in groups:
+        if g['name'] in egroups:
+            g['value_type'] = get_value_type(value_type)
+            g['value_init'] = get_value_init(value_type)
+
+
+def add_group_value(yaml_data):
+    for e in yaml_data['events']:
+        condition = lambda c: c if c else None
+        set_group_value_data(yaml_data['groups'],
+                             e['groups'],
+                             get_condition_type(
+                                 yaml_data['conditions'],
+                                 condition(e['triggers'][0]['conditions'][0])))
+
+
 def generate(yaml_file, output_file):
     with open(yaml_file, 'r') as yaml_input:
         yaml_data = yaml.safe_load(yaml_input) or {}
+
+    add_group_value(yaml_data)
 
     with open(output_file, 'w') as gen_out:
         gen_out.write(Template(filename='generated.mako.cpp').render(
