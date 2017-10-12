da5c97b5e64ad5605abf526c4efb282af32a4fde
Matt Spinler <spinler@us.ibm.com>
2017-10-12 16:48:07 -0500

    Remove getIndirectLabelEnv
    It is no longer used.
    
    Change-Id: I4713fbd9232dfeed045125cbb827b4fe98c53717
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- env.cpp
+++ env.cpp
@@ -81,45 +81,4 @@ std::string getIndirectID(
     return content;
 }
 
-std::string getIndirectLabelEnv(
-    const char* prefix, std::string path, const SensorSet::key_type& sensor)
-{
-    std::string key;
-    std::string value;
-
-    path.append(sensor.first);
-    path.append(sensor.second);
-    path.append(1, '_');
-    path.append(hwmon::entry::label);
-
-    std::ifstream handle(path.c_str());
-    if (handle.fail())
-    {
-        return value;
-    }
-
-    std::string content(
-        (std::istreambuf_iterator<char>(handle)),
-        (std::istreambuf_iterator<char>()));
-
-    if (content.empty())
-    {
-        return value;
-    }
-
-    content.pop_back();
-
-    key.assign(prefix);
-    key.append(1, '_');
-    key.append(sensor.first);
-    key.append(content);
-    auto env = getenv(key.c_str());
-    if (env)
-    {
-        value.assign(env);
-    }
-
-    return value;
-}
-
 // vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
