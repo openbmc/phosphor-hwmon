9f3abfa4da85c7f0358d86f890d8804bec8ee09f
Matt Spinler <spinler@us.ibm.com>
2017-10-12 16:48:07 -0500

    Remove getIndirectLabelEnv
    It is no longer used.
    
    Change-Id: I4713fbd9232dfeed045125cbb827b4fe98c53717
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- env.hpp
+++ env.hpp
@@ -38,19 +38,3 @@ std::string getEnv(
 std::string getIndirectID(
     std::string path,
     const SensorSet::key_type& sensor);
-
-/** @brief Get the label for the sensor with a level of indirection.
- *
- *  Read the sensor number from the <path>/<item><X>_label file.<item> & <X> is
- *  populated from the sensor key. The sensor label is read from the environment
- *  variable <prefix>_<item><sensorNum>.
- *
- *  @param[in] prefix - Prefix of the environment variable.
- *  @param[in] path - Directory path of the label file.
- *  @param[in] sensor - Sensor details.
- *
- *  @return On success return the sensor label, in case of failure return empty
- *          string.
- */
-std::string getIndirectLabelEnv(
-    const char* prefix, std::string path, const SensorSet::key_type& sensor);
