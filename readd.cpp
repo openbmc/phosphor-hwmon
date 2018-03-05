147b0337b36e58604fbdd517f71f97a4dae725c1
Matt Spinler <spinler@us.ibm.com>
2018-03-05 12:07:46 -0600

    Find hwmon path based on input path type
    If the input path starts with /devices, find the hwmon path
    using the findHwmonFromDevPath function, otherwise use
    findHwmonFromOFPath.
    
    Also change the argument name to something more generic.
    
    Did not change the argument's short name so the service
    file isn't coupled to this change.
    
    Tested: Tested with both paths.
    
    Change-Id: I5feb8a897fc86117b102ce44584ed977282cb955
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- readd.cpp
+++ readd.cpp
@@ -34,10 +34,19 @@ int main(int argc, char** argv)
     auto options = std::make_unique<ArgumentParser>(argc, argv);
 
     // Parse out path argument.
-    auto path = (*options)["of-name"];
+    auto path = (*options)["dev-path"];
     if (path != ArgumentParser::empty_string)
     {
-        path = sysfs::findHwmonFromOFPath(path);
+        // This path may either be a device path (starts with
+        // /devices), or an open firmware device tree path.
+        if (path.substr(0, 8) == "/devices")
+        {
+            path = sysfs::findHwmonFromDevPath(path);
+        }
+        else
+        {
+            path = sysfs::findHwmonFromOFPath(path);
+        }
     }
 
     if (path == ArgumentParser::empty_string)
