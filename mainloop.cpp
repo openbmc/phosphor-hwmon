6792c31e836fcc4a2b9b683c18ed9b587b7728bb
Matthew Barth <msbarth@us.ibm.com>
2018-03-26 09:55:19 -0500

    Re-add removed sensors during monitoring
    When a sensor returns a defined return code causing it to be removed
    during the monitoring loop, it should be re-added once the sensor is
    successfully read not producing any of the defined return codes. Once
    the sensor is re-added, it is erased from the removed list of sensors.
    When a sensor continues to return a defined return code, it remains in
    the removed sensor list and is attempted to be added after each
    completion of the monitoring loop.
    
    Tested:
        Verify sensor is removed when return code received during monitoring
        Sensor object added after successful read with no return codes
    
    Resolves openbmc/openbmc#2327
    
    Change-Id: Id286d4b1f1e11e1148ad4182dd9726d3c348c9de
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- mainloop.cpp
+++ mainloop.cpp
@@ -509,8 +509,6 @@ void MainLoop::run()
     // Polling loop.
     while (!_shutdown)
     {
-        // Used in marking a sensor for removal from dbus
-        std::vector<SensorSet::key_type> destroy;
         // Iterate through all the sensors.
         for (auto& i : state)
         {
@@ -578,7 +576,7 @@ void MainLoop::run()
                         {
                             // Return code found in sensor removal list
                             // Mark this sensor to be removed from dbus
-                            destroy.push_back(i.first);
+                            rmSensors[i.first] = std::get<0>(i.second);
                             continue;
                         }
                     }
@@ -602,7 +600,7 @@ void MainLoop::run()
                             entry("FILE=%s", file.c_str()));
 
 #ifdef REMOVE_ON_FAIL
-                    destroy.push_back(i.first);
+                    rmSensors[i.first] = std::get<0>(i.second);
 #else
                     exit(EXIT_FAILURE);
 #endif
@@ -611,9 +609,9 @@ void MainLoop::run()
         }
 
         // Remove any sensors marked for removal
-        for (auto& i : destroy)
+        for (auto& i : rmSensors)
         {
-            state.erase(i);
+            state.erase(i.first);
         }
 
         // Respond to DBus
