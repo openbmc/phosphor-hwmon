fc1834294c27af97282172b07e8da0846280ce76
Matthew Barth <msbarth@us.ibm.com>
2018-04-18 14:53:01 -0500

    Add status interface to sensors
    When a fault sysfs file is present for a sensor, the OperationalStatus
    interface is attached to the sensor object. The functional property is
    initially set to the corresponding value read from the fault sysfs file
    when the sensor object is created. A follow-up commit will address
    updating the functional property based on reading the fault sysfs file
    during the polling interval.
    
    Tested:
        OperationalStatus interface created for sensors with fault files
        Interface not created for sensors without fault file
        Functional property set to correct value from sensor's fault file
    
    Change-Id: Id75b3711d048d4667d2173a3255512cf5482ab67
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- mainloop.cpp
+++ mainloop.cpp
@@ -275,6 +275,75 @@ auto addValue(const SensorSet::key_type& sensor,
     return iface;
 }
 
+auto addStatus(const SensorSet::key_type& sensor,
+               const sysfs::hwmonio::HwmonIO& ioAccess,
+               const std::string& devPath,
+               ObjectInfo& info)
+{
+    namespace fs = std::experimental::filesystem;
+
+    std::shared_ptr<StatusObject> iface = nullptr;
+    static constexpr bool deferSignals = true;
+    auto& bus = *std::get<sdbusplus::bus::bus*>(info);
+    auto& objPath = std::get<std::string>(info);
+    auto& obj = std::get<Object>(info);
+
+    // Check if fault sysfs file exists
+    std::string sysfsFullPath;
+    std::string faultName = sensor.first;
+    std::string faultID = sensor.second;
+    std::string entry = hwmon::entry::fault;
+
+    sysfsFullPath = sysfs::make_sysfs_path(ioAccess.path(),
+                                           faultName,
+                                           faultID,
+                                           entry);
+    if (fs::exists(sysfsFullPath))
+    {
+        bool functional = true;
+        uint32_t fault = 0;
+        try
+        {
+            fault = ioAccess.read(faultName,
+                                  faultID,
+                                  entry,
+                                  sysfs::hwmonio::retries,
+                                  sysfs::hwmonio::delay);
+            if (fault != 0)
+            {
+                functional = false;
+            }
+        }
+        catch (const std::system_error& e)
+        {
+            using namespace phosphor::logging;
+            using namespace sdbusplus::xyz::openbmc_project::
+                Sensor::Device::Error;
+            using metadata = xyz::openbmc_project::Sensor::
+                Device::ReadFailure;
+
+            report<ReadFailure>(
+                    metadata::CALLOUT_ERRNO(e.code().value()),
+                    metadata::CALLOUT_DEVICE_PATH(devPath.c_str()));
+
+            log<level::INFO>("Logging failing sysfs file",
+                    phosphor::logging::entry(
+                            "FILE=%s", sysfsFullPath.c_str()));
+        }
+
+        iface = std::make_shared<StatusObject>(
+                bus,
+                objPath.c_str(),
+                deferSignals);
+        // Set functional property
+        iface->functional(functional);
+
+        obj[InterfaceType::STATUS] = iface;
+    }
+
+    return iface;
+}
+
 /**
  * Reads the environment parameters of a sensor and creates an object with
  * atleast the `Value` interface, otherwise returns without creating the object.
@@ -416,6 +485,9 @@ void MainLoop::getObject(SensorSet::container_t::const_reference sensor)
         target->enable();
     }
     addTarget<hwmon::FanPwm>(sensor.first, ioAccess, _devPath, info);
+
+    // Add status interface based on _fault file being present
+    addStatus(sensor.first, ioAccess, _devPath, info);
 
     // All the interfaces have been created.  Go ahead
     // and emit InterfacesAdded.
