b400e984f8a2694350b60171e446dba46e21d0cd
Patrick Venture <venture@google.com>
2018-04-17 17:38:02 -0700

    Update configure.ac to fix gtest
    Updated the configure.ac to match the example and this
    fixed gtest.
    
    Change-Id: I246f5e4da257a0475c69505fd357d7441087b525
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ sensor.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+
+#include "hwmon.hpp"
+#include "interface.hpp"
+
+namespace sensor {
+
+static constexpr auto typeAttrMap =
+{
+    // 1 - hwmon class
+    // 2 - unit
+    // 3 - sysfs scaling factor
+    // 4 - namespace
+    std::make_tuple(
+        hwmon::type::ctemp,
+        ValueInterface::Unit::DegreesC,
+        -3,
+        "temperature"),
+    std::make_tuple(
+        hwmon::type::cfan,
+        ValueInterface::Unit::RPMS,
+        0,
+        "fan_tach"),
+    std::make_tuple(
+        hwmon::type::cvolt,
+        ValueInterface::Unit::Volts,
+        -3,
+        "voltage"),
+    std::make_tuple(
+        hwmon::type::ccurr,
+        ValueInterface::Unit::Amperes,
+        -3,
+        "current"),
+    std::make_tuple(
+        hwmon::type::cenergy,
+        ValueInterface::Unit::Joules,
+        -6,
+        "energy"),
+    std::make_tuple(
+        hwmon::type::cpower,
+        ValueInterface::Unit::Watts,
+        -6,
+        "power"),
+};
+
+auto getHwmonType(decltype(typeAttrMap)::const_reference attrs)
+{
+    return std::get<0>(attrs);
+}
+
+auto getUnit(decltype(typeAttrMap)::const_reference attrs)
+{
+    return std::get<1>(attrs);
+}
+
+auto getScale(decltype(typeAttrMap)::const_reference attrs)
+{
+    return std::get<2>(attrs);
+}
+
+auto getNamespace(decltype(typeAttrMap)::const_reference attrs)
+{
+    return std::get<3>(attrs);
+}
+
+using AttributeIterator = decltype(*typeAttrMap.begin());
+using Attributes
+    = std::remove_cv<std::remove_reference<AttributeIterator>::type>::type;
+
+/** @brief Get Attribute tuple for the type
+ *
+ *  Given a type, it tries to find the corresponding tuple
+ *
+ *  @param[in] type the sensor type
+ *  @param[in,out] A pointer to the Attribute tuple
+ */
+auto getAttributes(const std::string& type, Attributes& attributes);
+
+}  // sensor
+
+// vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
