1a956d235fafded7f466b048bf7a9a4306e5a1fb
Patrick Venture <venture@google.com>
2018-04-20 20:07:23 -0700

    use hwmonio interface for HwmonIO injection
    Now FanSpeed and FanPwm objects expect to be given their
    HwmonIo objects.
    
    Note: Still working out what to test, AddTarget<...> is the goal,
    given a set of parameters, verify it handles it properly, and verify
    new values to/from FanSpeed, FanPwm move properly.  Also, need to add
    tests to verify the thresholds...
    
    Change-Id: I24a3dce2c4e25236af6126b004106e169fb185bd
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ test/hwmonio_mock.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+
+#include "gmock/gmock.h"
+
+#include "hwmonio.hpp"
+
+
+namespace hwmonio {
+
+class HwmonIoMock : public HwmonIoInterface {
+  public:
+    virtual ~HwmonIoMock() {};
+
+    MOCK_CONST_METHOD6(read,
+                       int64_t(const std::string&,
+                               const std::string&,
+                               const std::string&,
+                               size_t,
+                               std::chrono::milliseconds,
+                               bool));
+    MOCK_CONST_METHOD6(write,
+                       void(uint32_t,
+                            const std::string&,
+                            const std::string&,
+                            const std::string&,
+                            size_t,
+                            std::chrono::milliseconds));
+    MOCK_CONST_METHOD0(path, std::string());
+};
+
+} // namespace hwmonio
