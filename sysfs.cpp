03f5b7c786b7b19be4e60376181d2e59233a0b3f
Patrick Venture <venture@google.com>
2018-10-13 13:25:15 -0700

    sysfs: transition to std::find_if
    [sysfs.cpp:228]: (style) Consider using std::find_if algorithm instead of a
    raw loop.
    
    Change-Id: Ie07ca5e32405fc7d27e85936377cec44291d3f94
    Signed-off-by: Patrick Venture <venture@google.com>

--- sysfs.cpp
+++ sysfs.cpp
@@ -221,13 +221,16 @@ std::string findHwmonFromDevPath(const std::string& devPath)
             path.replace(pos, 2, ":");
         }
 
-        for (const auto& hwmonInst : fs::directory_iterator(path))
+        auto testPath = [](const fs::directory_entry& d) {
+            return (d.path().filename().string().find("hwmon") !=
+                    std::string::npos);
+        };
+
+        auto dir_iter = fs::directory_iterator(path);
+        const auto& hwmonInst = std::find_if(dir_iter, end(dir_iter), testPath);
+        if (hwmonInst != end(dir_iter))
         {
-            if ((hwmonInst.path().filename().string().find("hwmon") !=
-                 std::string::npos))
-            {
-                return hwmonInst.path();
-            }
+            return (*hwmonInst).path();
         }
     }
     catch (const std::exception& e)
