d38645b4709482ea65ee8eb43869633d3ba01ef8
Patrick Venture <venture@google.com>
2018-12-19 08:59:15 -0800

    sensor: add gpio handle test variation
    Add a sensor constructor test where the gpio handle is required.
    
    Change-Id: I05050fb71a6287183ca94f974e965e3f58de9499
    Signed-off-by: Patrick Venture <venture@google.com>

--- test/sensor_unittest.cpp
+++ test/sensor_unittest.cpp
@@ -1,7 +1,9 @@
 #include "env_mock.hpp"
+#include "gpio_mock.hpp"
 #include "hwmonio_mock.hpp"
 #include "sensor.hpp"
 
+#include <gpioplus/test/sys.hpp>
 #include <memory>
 #include <utility>
 
@@ -14,6 +16,7 @@ class SensorTest : public ::testing::Test
     void SetUp() override
     {
         envIntf = nullptr;
+        gpioIntf = nullptr;
     }
 };
 
@@ -37,6 +40,54 @@ TEST_F(SensorTest, BasicConstructorTest)
     EXPECT_CALL(eMock, getEnv(Eq("GPIOCHIP"), Eq(sensorKey)))
         .WillOnce(Return(""));
     EXPECT_CALL(eMock, getEnv(Eq("GPIO"), Eq(sensorKey))).WillOnce(Return(""));
+
+    /* Always calls GAIN and OFFSET, can use ON_CALL instead of EXPECT_CALL */
+    EXPECT_CALL(eMock, getEnv(Eq("GAIN"), Eq(sensorKey))).WillOnce(Return(""));
+    EXPECT_CALL(eMock, getEnv(Eq("OFFSET"), Eq(sensorKey)))
+        .WillOnce(Return(""));
+    EXPECT_CALL(eMock, getEnv(Eq("REMOVERCS"), Eq(sensorKey)))
+        .WillOnce(Return(""));
+
+    auto sensor =
+        std::make_unique<sensor::Sensor>(sensorKey, hwmonio_mock.get(), path);
+    EXPECT_FALSE(sensor == nullptr);
+}
+
+TEST_F(SensorTest, SensorRequiresGpio)
+{
+    /* Constructor test with only the GPIO chip set. */
+
+    StrictMock<EnvMock> eMock;
+    envIntf = &eMock;
+
+    StrictMock<GpioHandleMock> gMock;
+    gpioIntf = &gMock;
+
+    /* The following piece of code can probably be copied above once it's
+     * working.
+     */
+    uint32_t lineOffset = 5;
+    unsigned int id = 0;
+    gpioplus::test::SysMock gpioplusSys;
+    gpioplus::Chip chip(id, &gpioplusSys);
+
+    std::vector<gpioplus::Handle::Line> lines = {{lineOffset, 0}};
+    gpioplus::HandleFlags flags(chip.getLineInfo(lineOffset).flags);
+    flags.output = true;
+    auto handleMocked = std::make_unique<gpioplus::Handle>(
+        chip, lines, flags, "phosphor-hwmon-test");
+
+    auto sensorKey = std::make_pair(std::string("temp"), std::string("5"));
+    std::unique_ptr<hwmonio::HwmonIOInterface> hwmonio_mock =
+        std::make_unique<hwmonio::HwmonIOMock>();
+    std::string path = "/";
+
+    EXPECT_CALL(eMock, getEnv(Eq("GPIOCHIP"), Eq(sensorKey)))
+        .WillOnce(Return("chipA"));
+    EXPECT_CALL(eMock, getEnv(Eq("GPIO"), Eq(sensorKey))).WillOnce(Return("5"));
+
+    EXPECT_CALL(gMock, build(Eq("chipA"), Eq("5")))
+        .WillOnce(Return(std::move(handleMocked)));
 
     /* Always calls GAIN and OFFSET, can use ON_CALL instead of EXPECT_CALL */
     EXPECT_CALL(eMock, getEnv(Eq("GAIN"), Eq(sensorKey))).WillOnce(Return(""));
