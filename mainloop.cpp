9f7c11eb3511273b3999de1d27f843427effaad6
Brandon Kim <brandonkim@google.com>
2019-06-20 12:18:24 -0700

    sensor: Add UPDATE_FUNCTIONAL_ON_FAIL and its behavior
    Add the build switch UPDATE_FUNCTIONAL_ON_FAIL. When enabled, sensor
    read failures will not exit the mainloop. Instead, mainloop will update
    the Functional property and skip the read of that sensor.
    
    This will ignore remove RCs implementation, similar to
    NEGATIVE_ERRNO_ON_FAIL.
    
    Tested: I was able to use busctl to read the Functional property of a
    custom driver to test with UPDATE_FUNCTIONAL_ON_FAIL defined.
    1. Negative values were reported, Functional was true
    2. Sensor reporting errors had stale values, Functional was set to false
    
    Resolves: openbmc/phosphor-hwmon#10
    Signed-off-by: Brandon Kim <brandonkim@google.com>
    Change-Id: I0984dad12250e9587ec36de2f9212de0b0e1cda6

--- mainloop.cpp
+++ mainloop.cpp
@@ -393,21 +393,19 @@ void MainLoop::read()
                 input = "";
             }
 
+            int64_t value;
+            auto& objInfo = std::get<ObjectInfo>(i.second);
+            auto& obj = std::get<InterfaceMap>(objInfo);
+            std::unique_ptr<sensor::Sensor>& sensor = _sensorObjects[i.first];
+
+            auto& statusIface = std::any_cast<std::shared_ptr<StatusObject>&>(
+                obj[InterfaceType::STATUS]);
+            // As long as addStatus is called before addValue, statusIface
+            // should never be nullptr.
+            assert(statusIface);
+
             try
             {
-                int64_t value;
-                auto& objInfo = std::get<ObjectInfo>(i.second);
-                auto& obj = std::get<InterfaceMap>(objInfo);
-                std::unique_ptr<sensor::Sensor>& sensor =
-                    _sensorObjects[i.first];
-
-                auto& statusIface =
-                    std::any_cast<std::shared_ptr<StatusObject>&>(
-                        obj[InterfaceType::STATUS]);
-                // As long as addStatus is called before addValue, statusIface
-                // should never be nullptr.
-                assert(statusIface);
-
                 if (sensor->hasFaultFile())
                 {
                     auto fault = _ioAccess->read(
@@ -430,6 +428,8 @@ void MainLoop::read()
                     value =
                         _ioAccess->read(i.first.first, i.first.second, input,
                                         hwmonio::retries, hwmonio::delay);
+                    // Set functional property to true if we could read sensor
+                    statusIface->functional(true);
 
                     value = sensor->adjustValue(value);
                 }
@@ -438,6 +438,12 @@ void MainLoop::read()
             }
             catch (const std::system_error& e)
             {
+#ifdef UPDATE_FUNCTIONAL_ON_FAIL
+                // If UPDATE_FUNCTIONAL_ON_FAIL is defined, set the functional
+                // property to false and skip the "Remove RCs" check below
+                statusIface->functional(false);
+                continue;
+#endif
                 auto file = sysfs::make_sysfs_path(
                     _ioAccess->path(), i.first.first, i.first.second,
                     hwmon::entry::cinput);
