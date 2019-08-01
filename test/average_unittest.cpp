e16febd738a2a938cf9c9f4d0c3a7e4afa73715b
Carol Wang <wangkair@cn.ibm.com>
2019-08-01 17:31:30 +0800

    Support averaging power values
    Support new env variables 'AVERAGE_power* = "true"' in hwmon config file.
    When this env variable is set, power value is the calculated average value.
    Otherwise, power value is from power*_input by default.
    
    average =
    (cur_average*cur_average_interval - pre_average*pre_average_interval) /
    (cur_average_interval - pre_average_interval)
    
    hwmon config example:
    AVERAGE_power2 = "true"
    AVERAGE_power3 = "true"
    AVERAGE_power4 = "true"
    
    Tested: Set AVERAGE_power* in p0 OCC hwmon conf but not in p1 OCC hwmon conf,
    then get power sensor info with restapi to check the values.
    1. The values of p0*power are all average values.
    2. The values of p1*power are all input values.
    
    Note:
    Delete $(CODE_COVERAGE_CPPFLAGS) in AM_CPPFLAGS in test/Makefile.am.
    This option will define NDEBUG during configuration, then assert in
    code doesn't work.
    
    Resolves: openbmc/openbmc#3187
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>
    Change-Id: I8d97a7b2905c79cd4f2c276b32e7f5590ffc0483

---
+++ test/average_unittest.cpp
@@ -0,0 +1,49 @@
+#include "average.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using ::testing::Return;
+
+TEST(SensorKeyTest, InvalidSensorKey)
+{
+    Average av;
+
+    av.setAverageValue(std::make_pair("power", "0"), std::make_pair(0L, 0L));
+    av.setAverageValue(std::make_pair("power", "1"), std::make_pair(0L, 0L));
+
+    EXPECT_FALSE(av.getAverageValue(std::make_pair("power", "4")));
+}
+
+TEST(SensorKeyTest, ValidSensorKey)
+{
+    Average av;
+
+    av.setAverageValue(std::make_pair("power", "0"), std::make_pair(0L, 0L));
+    av.setAverageValue(std::make_pair("power", "1"), std::make_pair(2L, 2L));
+
+    auto value = av.getAverageValue(std::make_pair("power", "1"));
+    EXPECT_TRUE(value == std::make_pair(2L, 2L));
+}
+
+TEST(AverageTest, ZeroDelta)
+{
+    Average av;
+
+    EXPECT_FALSE(av.calAverage(1L, 1L, 2L, 1L));
+}
+
+TEST(AverageTest, NegativeDelta)
+{
+    Average av;
+
+    ASSERT_DEATH(av.calAverage(1L, 1L, 2L, 0L), "");
+}
+
+TEST(AverageTest, RightAverage)
+{
+    Average av;
+
+    EXPECT_TRUE(38837438L == av.calAverage(27624108L, 132864155500L, 27626120L,
+                                           132887999500L));
+}
