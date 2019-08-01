5a7fa50c7db2ae33e06772d8a9f64b30f7e58456
Carol Wang <wangkair@cn.ibm.com>
2019-08-01 17:31:30 +0800

    Support averaging power values
    Support new env variables 'AVERAGE_power* = "true"' in hwmon config file.
    When this env variable is set, power value is the calculated average value.
    Otherwise, power value is from power*_input by default.
    
    average =
    (cur_average*cur_average_interval - pre_average*pre_average_interval) /
    (cur_average_interval - pre_average_interval)
    
    hwmon config example:
    AVERAGE_power2 = "true"
    AVERAGE_power3 = "true"
    AVERAGE_power4 = "true"
    
    Tested: Set AVERAGE_power* in p0 OCC hwmon conf but not in p1 OCC hwmon conf,
    then get power sensor info with restapi to check the values.
    1. The values of p0*power are all average values.
    2. The values of p1*power are all input values.
    
    Resolves: openbmc/openbmc#3187
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>
    Change-Id: I8d97a7b2905c79cd4f2c276b32e7f5590ffc0483

---
+++ test/env_unittest.cpp
@@ -0,0 +1,36 @@
+#include "env.hpp"
+#include "env_mock.hpp"
+#include "util.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using ::testing::Return;
+using ::testing::StrEq;
+using ::testing::StrictMock;
+
+TEST(EnvTest, EmptyEnv)
+{
+    EXPECT_FALSE(
+        phosphor::utility::isAverageEnvSet(std::make_pair("power", "1")));
+}
+
+TEST(EnvTest, ValidAverageEnv)
+{
+    StrictMock<EnvMock> eMock;
+    envIntf = &eMock;
+
+    std::string power = "power";
+    std::string one = "1";
+    std::string two = "2";
+
+    EXPECT_CALL(eMock, getEnv(StrEq("AVERAGE"), power, one))
+        .WillOnce(Return("true"));
+    EXPECT_CALL(eMock, getEnv(StrEq("AVERAGE"), power, two))
+        .WillOnce(Return("bar"));
+
+    EXPECT_TRUE(
+        phosphor::utility::isAverageEnvSet(std::make_pair(power, one)));
+    EXPECT_FALSE(
+        phosphor::utility::isAverageEnvSet(std::make_pair(power, two)));
+}
