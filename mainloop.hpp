6d50c3e9e184d3741eec13bd164518864fe806bf
Brandon Kim <brandonkim@google.com>
2019-08-09 15:38:53 -0700

    sensor: Implement sensor "ASYNC_READ_TIMEOUT"
    This change will prevent sensors from blocking all other sensor reads
    and D-Bus if they do not report failures quickly enough.
    
    If "ASYNC_READ_TIMEOUT" environment variable is defined in the
    sensor's config file for a key_type, the sensor read will be
    asynchronous with timeout set in milliseconds.
    
    For example for "sensor1":
    ASYNC_READ_TIMEOUT_sensor1="1000"  // Timeout will be set to 1 sec
    
    If the read times out, the sensor read will be skipped and the
    sensor's functional property will be set to 'false'. Timed out futures
    will be placed in a map to prevent their destructor from running and
    blocking until the read completes (limitation of std::async).
    
    Tested: This patch has been running downstream for over a year to
            solve a slow I2C sensor reads causing IPMI slowdown.
    
    Change-Id: I3d9ed4d5c9cc87d3196fc281451834f3001d0b48
    Signed-off-by: Brandon Kim <brandonkim@google.com>

--- mainloop.hpp
+++ mainloop.hpp
@@ -9,6 +9,7 @@
 #include "types.hpp"
 
 #include <any>
+#include <future>
 #include <memory>
 #include <optional>
 #include <sdbusplus/server.hpp>
@@ -120,6 +121,8 @@ class MainLoop
     /** @brief Store the specifications of sensor objects */
     std::map<SensorSet::key_type, std::unique_ptr<sensor::Sensor>>
         _sensorObjects;
+    /** @brief Store the async futures of timed out sensor objects */
+    sensor::TimedoutMap _timedoutMap;
 
     /**
      * @brief Map of removed sensors
