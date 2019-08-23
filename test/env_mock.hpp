9e6e6bfbba74a7ab5fe125fcfe0e4e010b23935c
Kun Yi <kunyi731@gmail.com>
2019-08-23 10:26:49 -0700

    Use fake env class
    Add a fake class such that unit tests can set the contents
    of a fake env, and point mock class to delegate calls to the
    fake env. Rework the existing unit tests.
    
    This makes tests use as close as possible to a real env file,
    and that call flow in env::getEnv() is tested against it.
    
    Tested: unit tests still pass
    
    Change-Id: I0312d4218daa245ea41f301a72e9ac158a4450ed
    Signed-off-by: Kun Yi <kunyi731@gmail.com>

--- test/env_mock.hpp
+++ test/env_mock.hpp
@@ -2,6 +2,7 @@
 
 #include "env.hpp"
 
+#include <sstream>
 #include <string>
 
 #include <gmock/gmock.h>
@@ -9,10 +10,73 @@
 namespace env
 {
 
+/**
+ * @class: a fake std::env class
+ * @brief: trivially implements a keyval map whose value can be injected
+ */
+class FakeEnv : public Env
+{
+  public:
+    /**
+     * @brief: parse the input string into key/pair map.
+     *         Each line of string constructor is splitted by the first '=' to
+     *         produce the key/val pair.
+     * @param[in] envStr: input string to the ctor. Expected to be in the format
+     *         of:
+     *
+     *         ENV1=abc\n
+     *         ENV2=def\n
+     */
+    void SetEnv(const std::string envStr)
+    {
+        std::stringstream i;
+        std::string line;
+
+        _keyVals.clear();
+        i << envStr;
+        while (std::getline(i, line))
+        {
+
+            if (line.empty())
+            {
+                continue;
+            }
+            auto pos = line.find('=');
+            _keyVals[line.substr(0, pos)] = line.substr(pos + 1);
+        }
+    }
+
+    const char* get(const char* key) const override
+    {
+        if (_keyVals.find(key) == _keyVals.end())
+            return nullptr;
+        return _keyVals.at(std::string(key)).c_str();
+    }
+
+  private:
+    std::map<std::string, std::string> _keyVals;
+};
+
 class EnvMock : public Env
 {
   public:
+<<<<<<< HEAD
     MOCK_CONST_METHOD1(get, const char*(const char*));
+=======
+    MOCK_CONST_METHOD1(get, std::string(const char*));
+
+    /* Set this before each test */
+    void setFakeEnv(const std::string& envStr)
+    {
+        _fake.SetEnv(envStr);
+        ON_CALL(*this, get).WillByDefault([this](const char* key) {
+            return _fake.get(key);
+        });
+    }
+
+  private:
+    FakeEnv _fake;
+>>>>>>> 67da828... Use fake env class
 };
 
 static inline EnvMock mockEnv;
